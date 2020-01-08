cb3a7e97f453ebe76a0b7825cc361275086371fc
Doug Broad <dbroad@osisoft.com>
2020-01-08 08:06:37 -0500

    Add warning regarding web server

--- basic_samples/Authentication/AuthorizationCodeFlow/DotNet/AuthorizationCodeFlow/Program.cs
+++ basic_samples/Authentication/AuthorizationCodeFlow/DotNet/AuthorizationCodeFlow/Program.cs
@@ -7,6 +7,7 @@ namespace AuthorizationCodeFlow
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("WARNING: The web server used in this sample is intended for use in testing or debugging sample applications locally. It has not been reviewed for security issues.");
             CreateWebHostBuilder(args).Build().Run();
         }
 
