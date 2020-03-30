84e638f418a5f4a5425ca4e0cf1465ecde477e47
Doug Broad <dbroad@osisoft.com>
2020-03-30 08:10:13 -0400

    Prettier, remove file headers

--- basic_samples/SDS/JavaScript/NodeJs/spec/Test.js
+++ basic_samples/SDS/JavaScript/NodeJs/spec/Test.js
@@ -1,17 +1,17 @@
-describe("Sds", function() {
+describe('Sds', function() {
   var Sample = require('../Sample');
 
-  beforeEach(function() {
-  });
+  beforeEach(function() {});
 
-  it("should be able to complete the main method", function() {
+  it('should be able to complete the main method', function() {
     console.log(Sample);
-    sample = Sample(null, null).then(response => {
-      v = true;
-      console.log(response);
-    }).catch(function (err) { 
-      console.log(err); 
-    });
+    sample = Sample(null, null)
+      .then(response => {
+        v = true;
+        console.log(response);
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
   });
-
 });
