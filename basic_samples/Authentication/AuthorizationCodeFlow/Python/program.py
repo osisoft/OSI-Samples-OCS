191962ea7e56bd9b28d07e9b965bcfb5acd006a0
Doug Broad <dbroad@osisoft.com>
2020-04-16 07:32:08 -0400

    Move selenium script to test file

--- basic_samples/Authentication/AuthorizationCodeFlow/Python/program.py
+++ basic_samples/Authentication/AuthorizationCodeFlow/Python/program.py
@@ -14,16 +14,13 @@ import hashlib
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import json
 import secrets
-import time
 from urllib.parse import urlparse, parse_qs
 import webbrowser
 
 import requests
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 
 
-def main(test=False):
+def main(test_script=None):
     """Main sample script
     Performs Authorization Code + PKCE Authentication against OSIsoft Cloud Services.
     If test=True, will use Selenium to perform browser authentication automatically.
@@ -89,47 +86,8 @@ def main(test=False):
             '&scope=' + scope + \
             '&acr_values=tenant:' + tenant_id
 
-        if test:
-            # Get Config
-            username = config.get('Test', 'Username')
-            password = config.get('Test', 'Password')
-
-            # Open Chrome Webdriver, go to Auth page
-            print()
-            print('Selenium 1: Open Chrome WebDriver')
-            chrome_options = Options()
-            chrome_options.add_argument("--headless")
-            chrome_options.add_argument("--no-sandbox")
-            browser = webdriver.Chrome(options=chrome_options)
-            browser.get(auth_url)
-            time.sleep(2)
-
-            # Use Personal Account (Must be enabled on Tenant)
-            print()
-            print('Selenium 2: Choose Personal Account')
-            browser.find_element_by_xpath(
-                xpath='descendant::a[@title="Personal Account"]').click()
-            time.sleep(2)
-
-            # Enter Username and submit
-            print()
-            print('Selenium 3: Enter Username')
-            browser.find_element_by_xpath(
-                '//*[@id="i0116"]').send_keys(username)
-            browser.find_element_by_xpath('//*[@id="idSIButton9"]').click()
-            time.sleep(2)
-
-            # Enter Password and submit
-            print()
-            print('Selenium 4: Enter Password')
-            browser.find_element_by_xpath(
-                '//*[@id="i0118"]').send_keys(password)
-            elem = browser.find_element_by_xpath('//*[@id="idSIButton9"]')
-            try:
-                browser.set_page_load_timeout(2)
-                elem.click()
-            except Exception:
-                print('Ignore time out, start the server...')
+        if test_script:
+            test_script(auth_url)
         else:
             # Open user default web browser at Auth page
             webbrowser.open(auth_url)
@@ -154,7 +112,7 @@ def main(test=False):
         print('Step 6: Read the Access Token:')
         print(token)
 
-        if test:
+        if test_script:
             assert token, "Failed to obtain access token"
 
         print()
