f12b24341d6a070cc85d5d84428cbfbb2d7da3a5
Derek Endres <dendres@osisoft.com>
2020-05-04 09:55:41 -0400

    more updates

--- advanced_samples/CSV/dotnet/CSVtoOCSTest/UnitTest1.cs
+++ advanced_samples/CSV/dotnet/CSVtoOCSTest/UnitTest1.cs
@@ -1,9 +1,15 @@
-using Xunit;
-
 namespace CSVtoOCSTest
 {
+    using Xunit;
+
+    /// <summary>
+    /// Holds all of the tests.
+    /// </summary>
     public class UnitTest1
     {
+        /// <summary>
+        /// Simple E2E test.
+        /// </summary>
         [Fact]
         public void Test1()
         {
