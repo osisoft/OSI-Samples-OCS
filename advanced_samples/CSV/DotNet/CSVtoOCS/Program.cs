f4b333ea0190722efa38204dc8f5008d44261da1
Doug Broad <dbroad@osisoft.com>
2020-05-05 10:39:58 -0400

    Add Polaris to CSVtoOCS

--- advanced_samples/CSV/DotNet/CSVtoOCS/Program.cs
+++ advanced_samples/CSV/DotNet/CSVtoOCS/Program.cs
@@ -1,71 +1,68 @@
-﻿
-using Microsoft.Extensions.Configuration;
-using Newtonsoft.Json;
-using OSIsoft.Data;
-using OSIsoft.Data.Reflection;
-using OSIsoft.Identity;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
-using System.Globalization;
 using CsvHelper;
-using IdentityModel.Client;
-using IdentityModel.OidcClient;
+using Microsoft.Extensions.Configuration;
+using OSIsoft.Data;
+using OSIsoft.Data.Reflection;
 
 namespace CSVtoOCS
 {
-    public class Program
+    public static class Program
     {
-        public static Exception toThrow = null;
+        private const string _stream1ID = "stream1";
+        private const string _stream2ID = "stream2";
+        private const string _typeID = "TemperatureReadings";
 
-        static List<TemperatureReadingsWithIds> dataList;
-        static IEnumerable<string> streamsIdsToSendTo;
-        static ISdsDataService dataService;
-        static ISdsMetadataService metaService;
-        private static bool createStreams = true;
-        static string stream1ID = "stream1", stream2ID = "stream2", typeID = "TemperatureReadings";
-        static bool test = false;
-        static IConfiguration configuration;
+        private static Exception _toThrow = null;
+        private static List<TemperatureReadingsWithIds> _dataList;
+        private static IEnumerable<string> _streamsIdsToSendTo;
+        private static ISdsDataService _dataService;
+        private static ISdsMetadataService _metaService;
+        private static IConfiguration _configuration;
 
-        static void Main(string[] args)
+        private static bool CreateStreams { get; set; } = true;
+
+        public static void Main(string[] args)
         {
             string fileLocationIn = "datafile.csv";
-            if (args.Length > 0)
+            if (args != null && args.Length > 0)
             {
                 fileLocationIn = args[0];
             }
+
             MainAsync(fileLocation: fileLocationIn).GetAwaiter().GetResult();
         }
 
-        public static async Task<bool> MainAsync(bool testIn = false, string fileLocation = "datafile.csv")
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "Sample needs to ensure cleanup, and will throw last error encountered.")]
+        public static async Task<bool> MainAsync(bool test = false, string fileLocation = "datafile.csv")
         {
-            test = testIn;
-
             try
             {
                 // Import data in.  Use csv reader and custom class to make it simple
                 using (var reader = new StreamReader(fileLocation))
                 using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
                 {
-                    dataList = csv.GetRecords<TemperatureReadingsWithIds>().ToList();
+                    _dataList = csv.GetRecords<TemperatureReadingsWithIds>().ToList();
                 }
-                // Use Linq to get the distinct StreamIds we need.  
-                streamsIdsToSendTo = dataList.Select(dataeEntry => dataeEntry.StreamId).Distinct();
 
-                //Get Configuration information about where this is sending to
-                configuration = new ConfigurationBuilder()
+                // Use Linq to get the distinct StreamIds we need.  
+                _streamsIdsToSendTo = _dataList.Select(dataeEntry => dataeEntry.StreamId).Distinct();
+
+                // Get Configuration information about where this is sending to
+                _configuration = new ConfigurationBuilder()
                     .SetBasePath(Directory.GetCurrentDirectory())
                     .AddJsonFile("appsettings.json")
                     .AddJsonFile("appsettings.test.json", optional: true)
                     .Build();
 
-                var tenantId = configuration["TenantId"];
-                var namespaceId = configuration["NamespaceId"];
-                var resource = configuration["Resource"];
-                var clientId = configuration["ClientID"];
+                var tenantId = _configuration["TenantId"];
+                var namespaceId = _configuration["NamespaceId"];
+                var resource = _configuration["Resource"];
+                var clientId = _configuration["ClientID"];
 
                 if (!test)
                 {
@@ -73,88 +70,91 @@ namespace CSVtoOCS
                 }
                 else
                 {
-                    SystemBrowser.Password = configuration["Password"];
-                    SystemBrowser.UserName = configuration["UserName"];
+                    SystemBrowser.Password = _configuration["Password"];
+                    SystemBrowser.UserName = _configuration["UserName"];
                 }
 
-                (configuration as ConfigurationRoot).Dispose();
+                (_configuration as ConfigurationRoot).Dispose();
 
                 // Setup access to OCS
-                AuthenticationHandler_PKCE authenticationHandler = new AuthenticationHandler_PKCE(tenantId, clientId, resource);
+                AuthenticationHandlerPKCE authenticationHandler = new AuthenticationHandlerPKCE(tenantId, clientId, resource);
 
                 SdsService sdsService = new SdsService(new Uri(resource), authenticationHandler);
-                dataService = sdsService.GetDataService(tenantId, namespaceId);
-                metaService = sdsService.GetMetadataService(tenantId, namespaceId);
+                _dataService = sdsService.GetDataService(tenantId, namespaceId);
+                _metaService = sdsService.GetMetadataService(tenantId, namespaceId);
 
-                if (createStreams)
+                if (CreateStreams)
                 {
                     SdsType typeToCreate = SdsTypeBuilder.CreateSdsType<TemperatureReadings>();
-                    typeToCreate.Id = typeID;
+                    typeToCreate.Id = _typeID;
                     Console.WriteLine("Creating Type");
-                    await metaService.GetOrCreateTypeAsync(typeToCreate);
-                    var stream1 = new SdsStream { Id = stream1ID, TypeId = typeToCreate.Id };
-                    var stream2 = new SdsStream { Id = stream2ID, TypeId = typeToCreate.Id };
+                    await _metaService.GetOrCreateTypeAsync(typeToCreate).ConfigureAwait(false);
+                    var stream1 = new SdsStream { Id = _stream1ID, TypeId = typeToCreate.Id };
+                    var stream2 = new SdsStream { Id = _stream2ID, TypeId = typeToCreate.Id };
                     Console.WriteLine("Creating Stream");
-                    stream1 = await metaService.GetOrCreateStreamAsync(stream1);
-                    stream2 = await metaService.GetOrCreateStreamAsync(stream2);
+                    stream1 = await _metaService.GetOrCreateStreamAsync(stream1).ConfigureAwait(false);
+                    stream2 = await _metaService.GetOrCreateStreamAsync(stream2).ConfigureAwait(false);
                 }
 
                 Console.WriteLine("Sending Data");
+
                 // Loop over each stream to send to and send the data as one call.
-                foreach (string streamId in streamsIdsToSendTo)
+                foreach (string streamId in _streamsIdsToSendTo)
                 {
                     // Get all of the data for this stream in a list
-                    var valueToSend = dataList.Where(dataEntry => dataEntry.StreamId == streamId)  //gets only appropriate data for stream
+                    var valueToSend = _dataList.Where(dataEntry => dataEntry.StreamId == streamId) // gets only appropriate data for stream
                                               .Select(dataEntry => new TemperatureReadings(dataEntry)) // transforms it to the right data
                                               .ToList(); // needed in IList format for insertValues
-                    await dataService.InsertValuesAsync(streamId, valueToSend);
+                    await _dataService.InsertValuesAsync(streamId, valueToSend).ConfigureAwait(false);
                 }
 
                 if (test)
                 {
-                    //checks to make sure values are written
-                    await CheckValuesWrittenASync();
+                    // Checks to make sure values are written
+                    await CheckValuesWrittenASync().ConfigureAwait(false);
                 }
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
-                toThrow = ex;
+                _toThrow = ex;
             }
             finally
             {
                 if (test)
                 {
-                    if (!createStreams)
+                    if (!CreateStreams)
                     {
                         // if we just created the data lets just remove that
                         // Do Delete
                         Console.WriteLine("Deleting Data");
-                        await DeleteValuesAsync();
+                        await DeleteValuesAsync().ConfigureAwait(false);
+
                         // Do Delete check
-                        await CheckDeletesValuesAsync();
+                        await CheckDeletesValuesAsync().ConfigureAwait(false);
                     }
                     else
                     {
                         Console.WriteLine("Deleting Streams");
+
                         // if we created the types and streams, lets remove those too
-                        await RunInTryCatch(metaService.DeleteStreamAsync, stream1ID);
-                        await RunInTryCatch(metaService.DeleteStreamAsync, stream2ID);
+                        await RunInTryCatch(_metaService.DeleteStreamAsync, _stream1ID).ConfigureAwait(false);
+                        await RunInTryCatch(_metaService.DeleteStreamAsync, _stream2ID).ConfigureAwait(false);
                         Console.WriteLine("Deleting Types");
-                        await RunInTryCatch(metaService.DeleteTypeAsync, typeID);
+                        await RunInTryCatch(_metaService.DeleteTypeAsync, _typeID).ConfigureAwait(false);
 
                         // Check deletes
-                        await RunInTryCatchExpectException(metaService.GetStreamAsync, stream1ID);
-                        await RunInTryCatchExpectException(metaService.GetStreamAsync, stream2ID);
-                        await RunInTryCatchExpectException(metaService.GetTypeAsync, typeID);
+                        await RunInTryCatchExpectException(_metaService.GetStreamAsync, _stream1ID).ConfigureAwait(false);
+                        await RunInTryCatchExpectException(_metaService.GetStreamAsync, _stream2ID).ConfigureAwait(false);
+                        await RunInTryCatchExpectException(_metaService.GetTypeAsync, _typeID).ConfigureAwait(false);
                     }
                 }
             }
 
-            if (toThrow != null)
-                throw toThrow;
+            if (_toThrow != null)
+                throw _toThrow;
 
-            return toThrow == null;
+            return _toThrow == null;
         }
 
         /// <summary>
@@ -162,72 +162,72 @@ namespace CSVtoOCS
         /// </summary>
         /// <param name="methodToRun">The method to run.</param>
         /// <param name="value">The value to put into the method to run</param>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "Sample needs to ensure cleanup, and will throw last error encountered.")]
         private static async Task RunInTryCatch(Func<string, Task> methodToRun, string value)
         {
             try
             {
-                await methodToRun(value);
+                await methodToRun(value).ConfigureAwait(false);
             }
             catch (Exception ex)
             {
                 Console.WriteLine($"Got error in {methodToRun.Method.Name} with value {value} but continued on:" + ex.Message);
-                if (toThrow == null)
+                if (_toThrow == null)
                 {
-                    toThrow = ex;
+                    _toThrow = ex;
                 }
             }
         }
-
 
         /// <summary>
         /// Use this to run a method that you don't want to stop the program if there is an exception, and you expect an exception
         /// </summary>
         /// <param name="methodToRun">The method to run.</param>
         /// <param name="value">The value to put into the method to run</param>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "Method expects an exception from method.")]
         private static async Task RunInTryCatchExpectException(Func<string, Task> methodToRun, string value)
         {
             try
             {
-                await methodToRun(value);
+                await methodToRun(value).ConfigureAwait(false);
 
                 Console.WriteLine($"Got error.  Expected {methodToRun.Method.Name} with value {value} to throw an error but it did not:");
             }
-            catch
-            {
-
-            }
+            catch { }
         }
 
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "Sample needs to ensure cleanup, and will throw last error encountered.")]
         private static async Task CheckValuesWrittenASync()
         {
-            foreach (string streamId in streamsIdsToSendTo)
+            foreach (string streamId in _streamsIdsToSendTo)
             {
                 try
                 {
-                    var lastVal = await dataService.GetLastValueAsync<TemperatureReadings>(streamId);
-                    if (lastVal == null && toThrow == null)
+                    var lastVal = await _dataService.GetLastValueAsync<TemperatureReadings>(streamId).ConfigureAwait(false);
+                    if (lastVal == null && _toThrow == null)
                     {
                         throw new Exception($"Value for {streamId} was not found");
                     }
                 }
                 catch (Exception ex)
                 {
-                    if (toThrow == null)
+                    if (_toThrow == null)
                     {
-                        toThrow = ex;
+                        _toThrow = ex;
                     }
                 }
             }
         }
 
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "Sample needs to ensure cleanup, and will throw last error encountered.")]
         private static async Task CheckDeletesValuesAsync()
         {
-            foreach (string streamId in streamsIdsToSendTo)
+            foreach (string streamId in _streamsIdsToSendTo)
             {
                 try
                 {
-                    var lastVal = await dataService.GetLastValueAsync<TemperatureReadings>(streamId);
-                    if (lastVal != null && toThrow == null)
+                    var lastVal = await _dataService.GetLastValueAsync<TemperatureReadings>(streamId).ConfigureAwait(false);
+                    if (lastVal != null && _toThrow == null)
                     {
                         throw new Exception($"Value for {streamId} was found");
                     }
@@ -235,60 +235,33 @@ namespace CSVtoOCS
                 catch (Exception ex)
                 {
                     Console.WriteLine($"Got error in seeing that removed values are gone in {streamId} but continued on:" + ex.Message);
-                    if (toThrow == null)
+                    if (_toThrow == null)
                     {
-                        toThrow = ex;
+                        _toThrow = ex;
                     }
                 }
             }
         }
 
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "Sample needs to ensure cleanup, and will throw last error encountered.")]
         private static async Task DeleteValuesAsync()
         {
-            foreach (string streamId in streamsIdsToSendTo)
+            foreach (string streamId in _streamsIdsToSendTo)
             {
-
                 try
                 {
-                    var timeStampToDelete = dataList.Select(o => o.Timestamp);
-                    await dataService.RemoveValuesAsync(streamId, timeStampToDelete);
+                    var timeStampToDelete = _dataList.Select(o => o.Timestamp);
+                    await _dataService.RemoveValuesAsync(streamId, timeStampToDelete).ConfigureAwait(false);
                 }
                 catch (Exception ex)
                 {
                     Console.WriteLine($"Got error in removing values in {streamId} but continued on:" + ex.Message);
-                    if (toThrow == null)
+                    if (_toThrow == null)
                     {
-                        toThrow = ex;
+                        _toThrow = ex;
                     }
                 }
             }
-        }
-
-        public class TemperatureReadingsWithIds : TemperatureReadings
-        {
-            public string StreamId { get; set; }
-        }
-
-        public class TemperatureReadings
-        {
-            public TemperatureReadings()
-            {
-            }
-
-            public TemperatureReadings(TemperatureReadingsWithIds tempReading)
-            {
-                Timestamp = tempReading.Timestamp;
-                Temperature1 = tempReading.Temperature1;
-                Temperature2 = tempReading.Temperature2;
-                Code = tempReading.Code;
-            }
-
-
-            [SdsMember(IsKey = true)]
-            public DateTime Timestamp { get; set; }
-            public int Temperature1 { get; set; }
-            public double Temperature2 { get; set; }
-            public string Code { get; set; }
         }
     }
 }
--- advanced_samples/CSV/DotNet/CSVtoOCSTest/UnitTests.cs
+++ advanced_samples/CSV/DotNet/CSVtoOCSTest/UnitTests.cs
@@ -1,17 +1,17 @@
-namespace CSVtoOCSTest
+﻿namespace CSVtoOCSTest
 {
     using Xunit;
 
     /// <summary>
     /// Holds all of the tests.
     /// </summary>
-    public class UnitTest1
+    public class UnitTests
     {
         /// <summary>
         /// Simple E2E test.
         /// </summary>
         [Fact]
-        public void Test1()
+        public void CSVtoOCSTest()
         {
             CSVtoOCS.SystemBrowser.OpenBrowser = new OpenTestBrowser();
             Assert.True(CSVtoOCS.Program.MainAsync(true).Result);
